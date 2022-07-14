d59ba8a48f059a8c12ec0f110958c770a42a7c97
xingtuan <xingtuan@xtkjit.com>
2022-07-14 09:19:56 +0800

    宝马

--- src/com/xtkjit/gouzaoqi/car.java
+++ src/com/xtkjit/gouzaoqi/car.java
@@ -2,5 +2,13 @@ package com.xtkjit.gouzaoqi;
 
 public class car {
     String name;
-
+    double price;
+    public car(){
+        System.out.println("=无参数构造器被调用=");
+    }
+    public car(String n, double p){
+        System.out.println("=有参数构造器被调用=");
+        name = n;
+        price = p;
+    }
 }
--- src/com/xtkjit/gouzaoqi/test.java
+++ src/com/xtkjit/gouzaoqi/test.java
@@ -2,6 +2,10 @@ package com.xtkjit.gouzaoqi;
 
 public class test {
     public static void main(String[] args) {
-
+        car c = new car();
+        c.name = "宝马";
+        c.price = 38.9;
+        System.out.println(c.name);
+        System.out.println(c.price);
     }
 }
